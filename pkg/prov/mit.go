5ca81537d6f49e5f84186e5143904340d1c8c2fe
Kirill Sysoev <k.sysoev@me.com>
2025-05-13 20:59:31 +0800

    Add MIT provider and update config structure
    Introduce a new `prov` package with MIT provider implementation, including configuration and HTTP client setup. Extend the appConfig struct to include `MIT` and `Repo` configurations, ensuring alignment with the application's expanded functionality.

---
+++ pkg/prov/mit.go
@@ -0,0 +1,25 @@
+package prov
+
+import (
+	"net/http"
+	"time"
+)
+
+type Config struct {
+	Url string `mapstructure:"url"`
+}
+
+type MIT struct {
+	baseUrl string
+	cl      *http.Client
+}
+
+// New creates and returns a new instance of the MIT struct initialized with the provided configuration.
+func New(cfg *Config) *MIT {
+	return &MIT{
+		baseUrl: cfg.Url,
+		cl: &http.Client{
+			Timeout: 5 * time.Second,
+		},
+	}
+}
