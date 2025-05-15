0029894a0396ea42385a4f1a09ed16d053264021
Kirill Sysoev <k.sysoev@me.com>
2025-05-15 21:08:42 +0800

    Add token generation and storage logic to Service
    Implemented the `CreateToken` method to generate and store API tokens for users. Added logic to enforce a maximum token limit and handle related errors. Introduced a new `GetAPIKeys` method in the `UserRepo` interface and updated `Service` initialization.

--- pkg/core/svc.go
+++ pkg/core/svc.go
@@ -6,8 +6,13 @@ import (
 	"time"
 )
 
+var (
+	ErrMaxTokensExceeded = fmt.Errorf("maximum tokens exceeded")
+)
+
 type UserRepo interface {
 	AddAPIKey(ctx context.Context, userID string, apiKeyID string, expiresIn time.Duration) error
+	GetAPIKeys(ctx context.Context, userID string) ([]string, error)
 }
 
 type MITProv interface {
@@ -19,6 +24,7 @@ type Service struct {
 	prov MITProv
 }
 
+// New initializes and returns a new Service instance with the provided UserRepo and MITProv.
 func New(repo UserRepo, prov MITProv) *Service {
 	return &Service{
 		repo: repo,
@@ -26,6 +32,26 @@ func New(repo UserRepo, prov MITProv) *Service {
 	}
 }
 
-func (s *Service) CreateToken(_ context.Context, _ string) (*APIToken, error) {
-	return nil, fmt.Errorf("not implemented")
+// CreateToken generates a new API token for the specified user, storing it in the repository, if token limits are not exceeded.
+// Returns an error if the token limit is reached, fails to generate the token, or fails to save the token in the repository.
+func (s *Service) CreateToken(ctx context.Context, userID string) (*APIToken, error) {
+	keys, err := s.repo.GetAPIKeys(ctx, userID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get API keys: %w", err)
+	}
+
+	if len(keys) > 0 {
+		return nil, ErrMaxTokensExceeded
+	}
+
+	token, err := s.prov.GenerateToken()
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate token: %w", err)
+	}
+
+	if err = s.repo.AddAPIKey(ctx, userID, token.KeyID, time.Duration(token.ExpiresIn)*time.Second); err != nil {
+		return nil, fmt.Errorf("failed to add API key: %w", err)
+	}
+
+	return token, nil
 }
